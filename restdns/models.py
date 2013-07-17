4551cb1946c71ce23f4806aa005cb3d8f99e9493
Antoine Millet <antoine@inaps.org>
2013-07-17 21:15:30 +0200

    Removed unused imports

--- restdns/models.py
+++ restdns/models.py
@@ -2,7 +2,6 @@ import uuid
 
 from django.db import models
 from django.db.models.signals import pre_save, post_save, post_delete
-from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.dispatch import receiver
 
